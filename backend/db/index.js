b13a41773ccba51f4c9cb8d89a4648e9ca363538
annakhizhniak <kukurika.anna@gmail.com>
2024-07-25 20:01:59 +0300

    added mongoose Schemas for orders, items, comments

--- backend/db/index.js
+++ backend/db/index.js
@@ -2,28 +2,41 @@ const { dbUrl } = require('config');
 const { ObjectId } = require('mongodb');
 const mongoose = require('mongoose');
 
-// const AdminSchema = new mongoose.Schema({
-//   login: { type: String, unique: true },
-//   password: String
-// });
-
-// const PostSchema = new mongoose.Schema({
-//   name: { type: String, unique: true },
-//   description: String,
-//   date: String,
-//   author: {
-//     type: mongoose.Schema.Types.ObjectId,
-//     ref: 'users'
-//   },
-//   comments: [{
-//     type: mongoose.Schema.Types.ObjectId,
-//     ref: 'comments'
-//   }]
-// });
-
+const OrderSchema = new mongoose.Schema({
+  itemCode: { type: String },
+  date: { type: String },
+  timeCreated: { type: String},
+  dateCompleted: { type: String},
+  total: { type: Number},
+  totalItems: { type: Number},
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  items: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'items'
+  }]
+})
+const ItemSchema = new mongoose.Schema({
+  name: { type: String },
+  description: { type: String },
+  photo: { type: String },
+  price: { type: Number },
+  restBalance: { type: Number },
+  isSold: { type: Boolean },
+  isDiscount: { type: Boolean },
+  discountPercentage: { type: Number },
+  color: [{ type: String }],
+  rating: { type: Number },
+  comments: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'comments'
+  }],
+})
 const UserSchema = new mongoose.Schema({
-  name: { type: String},
-  login: {type: String, unique: true},
+  name: { type: String },
+  login: {type: String, unique: true },
   lastName: { type: String },
   surName: { type: String },
   birthDate: { type: String },
@@ -33,74 +46,68 @@ const UserSchema = new mongoose.Schema({
   bonuses: { type: String },
   mailing: { type: Boolean },
   password: { type: String },
-  passwordSubmit: {type: Boolean}
-
-
-  // posts: [{
-  //   type: mongoose.Schema.Types.ObjectId,
-  //   ref: 'posts'
-  // }],
-  // comments: [{
-  //   type: mongoose.Schema.Types.ObjectId,
-  //   ref: 'comments'
-  // }]
+  isPasswordSubmit: { type: Boolean },
+  favorites: { type: Array },
+  orders: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'orders'
+  }],
+  comments: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'comments'
+  }],
+});
+const CommentSchema = new mongoose.Schema({
+  text: { type: String },
+  date: { type: String },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  item: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'items'
+  }
 });
 
-// const CommentSchema = new mongoose.Schema({
-//   text: String,
-//   date: String,
-//   user: {
-//     type: mongoose.Schema.Types.ObjectId,
-//     ref: 'users'
-//   },
-//   post: {
-//     type: mongoose.Schema.Types.ObjectId,
-//     ref: 'posts'
-//   }
-// });
+UserSchema.pre('findOneAndDelete', async function(next) {
+  const query = this.getQuery();
 
+  try {
+    const comment = await Comment.findOne(query);
 
-// PostSchema.pre('findOneAndDelete', async function(next) {
-//   const query = this.getQuery();
-//
-//   try {
-//     const post = await Post.findOne(query);
-//
-//     if (!post) {
-//       return;
-//     }
-//
-//     await mongoose.model('comments').deleteMany( { _id: { $in: post.comments }} );
-//     next();
-//   } catch (error) {
-//     next(error);
-//   }
-// });
+    if (!comment) return;
 
+    const order = await Order.findOne(query);
 
-// UserSchema.pre('findOneAndDelete', async function(next) {
-//   const query = this.getQuery();
-//
-//   try {
-//     const user = await User.findOne(query);
-//
-//     if (!user) {
-//       return;
-//     }
-//
-//     await mongoose.model('posts').deleteMany({ _id: { $in: user.posts } });
-//     await mongoose.model('comments').deleteMany({ _id: { $in: user.comments } });
-//
-//     next();
-//   } catch (error) {
-//     next(error);
-//   }
-// });
+    if (!order) return;
 
-// const Admin = mongoose.model('admins', AdminSchema);
-// const Post = mongoose.model('posts', PostSchema);
+    await mongoose.model('comments').deleteMany({ _id: { $in: user.comments }})
+    await mongoose.model('orders').deleteMany({ _id: { $in: user.orders }})
+
+  } catch (error) {
+    next(error);
+  }
+
+})
+
+ItemSchema.pre('findOneAndDelete', async function(next) {
+  const query = this.getQuery();
+  try {
+    const item = await Item.findOne(query);
+
+    if (!item) return;
+
+    await mongoose.model('comments').deleteMany({ _id: { $in: item.comments }})
+  } catch (error) {
+    next(error);
+  }
+})
+
+const Item = mongoose.model('items', ItemSchema);
 const User = mongoose.model('users', UserSchema);
-// const Comment = mongoose.model('comments', CommentSchema);
+const Order = mongoose.model('orders', OrderSchema);
+const Comment = mongoose.model('comments', CommentSchema);
 
 async function init() {
   try {
@@ -118,4 +125,7 @@ init();
 module.exports = {
   ObjectId,
   User,
+  Order,
+  Comment,
+  Item
 };
