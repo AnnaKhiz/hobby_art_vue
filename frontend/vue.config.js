55b6a6fd930fb39b44b3b2d391f7933ccda8e481
annakhizhniak <kukurika.anna@gmail.com>
2025-04-10 14:12:55 +0300

    commented dev server

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -6,15 +6,15 @@ module.exports = defineConfig({
   // publicPath: process.env.NODE_ENV === 'production'
   //   ? 'https://anna-khizhniak.site/portfolio/hobby-art-new/'
   //   : '/',
-  devServer: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000/',
-        changeOrigin: true,
-        pathRewrite: { '^/api': '' },
-      },
-    },
-  },
+  // devServer: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'http://localhost:3000/',
+  //       changeOrigin: true,
+  //       pathRewrite: { '^/api': '' },
+  //     },
+  //   },
+  // },
 
   // devServer: {
   //   proxy: 'http://localhost:5000',
