69bdb1c2034fe2092df3d759881d6ed6d26f5adf
annakhizhniak <kukurika.anna@gmail.com>
2025-04-30 18:06:33 +0300

    moved contacts info inside headerInfocomponent

--- frontend/src/components/HeaderInfoComponent.vue
+++ frontend/src/components/HeaderInfoComponent.vue
@@ -1,15 +1,9 @@
 <template>
 	<section class="header__contacts">
 		<div class="container">
-			<ul class="header__contacts-list">
-        <ui-contacts
-          :contacts="contacts"
-        />
-			</ul>
-
+			<ui-contacts />
 		</div>
 	</section>
-
 </template>
 
 <script>
@@ -17,19 +11,6 @@ import UiContacts from "@/components/UI/uiContacts.vue";
 
 export default {
 	name: "HeaderInfoComponent",
-  components: {UiContacts},
-	data() {
-		return {
-			contacts: [
-				{ text: '+38 (044) 123 45 67', value: 'phone', href: 'tel:+380441234567', isLink: true },
-				{ text: 'г. Киев, ул. Шевченко, д. 6', value: 'address', isLink: false },
-				{ text: 'Ежедневно с 10:00 до 19:00', value: 'schedule', isLink: false },
-			]
-		}
-	}
+  components: { UiContacts },
 }
 </script>
-
-<style scoped lang="sass">
-
-</style>
--- frontend/src/components/UI/uiContacts.vue
+++ frontend/src/components/UI/uiContacts.vue
@@ -1,25 +1,26 @@
 <template>
-  <li
-      v-for="item in contacts"
-      :key="item.value"
-  >
-    <a v-if="item.isLink" :href="`${item.href}`" class="header__contacts-tel" >{{ item.text }}</a>
-    <span v-else>{{ item.text }}</span>
-  </li>
+	<ul class="header__contacts-list">
+		<li
+				v-for="item in contacts"
+				:key="item.value"
+		>
+			<a v-if="item.isLink" :href="`${item.href}`" class="header__contacts-tel" >{{ item.text }}</a>
+			<span v-else>{{ item.text }}</span>
+		</li>
+	</ul>
 </template>
 
 <script>
 export default {
   name: "uiContacts",
-  props: {
-    contacts: {
-      type: Array,
-      default: () => []
-    }
-  },
+	data() {
+		return {
+			contacts: [
+				{ text: '+38 (044) 123 45 67', value: 'phone', href: 'tel:+380441234567', isLink: true },
+				{ text: 'г. Киев, ул. Шевченко, д. 6', value: 'address', isLink: false },
+				{ text: 'Ежедневно с 10:00 до 19:00', value: 'schedule', isLink: false },
+			]
+		}
+	}
 }
 </script>
-
-<style scoped lang="sass">
-
-</style>
