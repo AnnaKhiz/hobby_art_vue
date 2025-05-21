ee6170548111a8eee8870a5915cbf26e53cb8266
annakhizhniak <kukurika.anna@gmail.com>
2025-05-21 14:50:42 +0300

    [frontend] created uiOrderInfoContainer component

--- frontend/src/components/user-page/UserPageHistory.vue
+++ frontend/src/components/user-page/UserPageHistory.vue
@@ -1,41 +1,11 @@
 <template>
   <div v-if="userOrdersList.length">
-    <div v-for="(order, index) in userOrdersList" :key="order._id" class="main__user-page-content-story-container" >
-      <div>
-        <div class="main__user-page-content-story-container-flex">
-          <div class="main__user-page-content-story-block">
-            <p class="main__user-page-content-story-desc">
-              {{ order.isOrderCopy ? 'КОПИЯ ' : '' }}Заказ № {{ order._id }} от {{ order.date }}
-            </p>
-            <p class="main__user-page-content-story-desc">
-              {{ order.totalQuantity}} товара на сумму {{ order.totalPrice }} грн
-            </p>
-            <a href="" class="main__user-page-content-story-link" @click.prevent="expandOrder(index)">
-              <span >Подробнее о заказе</span>
-							<img
-								src="@/assets/img/arrow-down.svg"
-								alt="arrow-down icon"
-								:style="`${order.show ? 'transform: rotate(180deg)' : 'transform: rotate(0)'}`"
-							>
-            </a>
-          </div>
-          <div class="main__user-page-content-story-block block-right">
-            <p class="main__user-page-content-story-desc right-block">
-              <span>Статус:</span>
-                {{ order.dateCompleted !== '' ? order.dateCompleted : 'В работе' }}
-            </p>
 
-            <a href="" class="main__user-page-content-story-repeat-link" @click.prevent="addNewOrderHandler(index)">
-              Повторить заказ
-            </a>
-          </div>
-
-        </div>
-				<!--      order list block -->
-        <ui-order-items-list :order="order" />
-      </div>
-
-    </div>
+		<ui-order-info-container
+			:user-orders-list="userOrdersList"
+			@update-orders-list="handleUpdate"
+			@update-show="handleExpand"
+		/>
 
     <!--  dialog notify -->
     <Transition name="fade">
@@ -51,11 +21,11 @@
 <script>
 import UiNotifyDialog from "@/components/UI/modal/uiNotifyDialog.vue";
 import { mapActions, mapGetters } from "vuex";
-import UiOrderItemsList from "@/components/UI/order/uiOrderItemsList.vue";
+import UiOrderInfoContainer from "@/components/UI/order/uiOrderInfoContainer.vue";
 
 export default {
   name: "userPageHistory",
-  components: { UiOrderItemsList, UiNotifyDialog },
+  components: {UiOrderInfoContainer, UiNotifyDialog },
   props: {
     user: {
       type: Object,
@@ -77,26 +47,16 @@ export default {
 		}),
 	},
   methods: {
-		...mapActions('order', ['addNewOrder', 'getUserOrdersList']),
-
-    expandOrder(index) {
-      this.userOrdersList[index].show = !this.userOrdersList[index].show
-    },
-
-    async addNewOrderHandler(index) {
-      const now = new Date(Date.now());
-      const options = { timeZone: 'Europe/Kiev', hour12: false };
-      this.date = now.toLocaleString('en-GB', options).replaceAll('/', '-');
-
-      let newOrderCopy = { ...this.userOrdersList[index], date: this.date };
-
-      delete newOrderCopy._id;
-			newOrderCopy.isOrderCopy = true;
-			await this.addNewOrder({ copy: true, body: newOrderCopy })
-
-      this.userOrdersList.push(this.orderItems);
-    },
-
+		...mapActions({
+			addNewOrder: 'order/addNewOrder',
+			getUserOrdersList: 'order/getUserOrdersList',
+		}),
+		handleUpdate(value) {
+			this.userOrdersList.push(value);
+		},
+		handleExpand(index) {
+			this.userOrdersList[index].show = !this.userOrdersList[index].show
+		},
     async initPage() {
 			await this.getUserOrdersList(this.user._id);
 
